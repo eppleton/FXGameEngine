516d201f66275240a3b7855013bb2fb076b9c79f
antonepple <antonepple@192.168.178.25>
2013-09-06 11:25:59 +0200

    fixed licenses

--- gameengine/src/main/java/de/eppleton/fx2d/timer/Pulse.java
+++ gameengine/src/main/java/de/eppleton/fx2d/timer/Pulse.java
@@ -1,6 +1,23 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This file is part of FXGameEngine 
+ * A Game Engine written in JavaFX
+ * Copyright (C) 2012 Anton Epple <info@eppleton.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. Look for COPYING file in the top folder.
+ * If not, see http://opensource.org/licenses/GPL-2.0.
+ * 
+ * For alternative licensing or use in closed source projects contact Anton Epple 
+ * <info@eppleton.de>
  */
 package de.eppleton.fx2d.timer;
 
